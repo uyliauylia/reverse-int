487ca9aaafbd4daf8be20da96ada77cd2df24fb1
Uylia <uyliauylia@yandex.ru>
2022-08-01 22:36:42 +0400

    feat: add solutions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
+    let i = 0;
+    let result = '';
+    n = Math.abs(n);
+    let str = String(n);
+    while (i < str.length) {
+        result = str[i] + result;
+        i = i+1;
+        }
+        return Number(result);
   
 }
